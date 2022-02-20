7b9fee797b2fc982a9e99a47a6a5315c9fbc4d2d
321-bip <dominicmarcos80@gmail.com>
2022-02-20 18:34:19 -0300

    fazendo funsao animaçao

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
+
 function corAleatoria(){
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
         return bgColor;
 };
 
-function minhasCrias(){
-    let cores =  corAleatoria()
-    let crias = document.querySelectorAll(".crias")
-        crias.forEach((elemento)=>{elemento.style.color = cores;})
-   //console.log(elemento)
-};
-   setInterval(minhasCrias, 100);
+function minhasCrias()
+ {
+    let meusBebes = document.querySelectorAll(".crias");
+        return {meusBebes};
+ };
+      
+function coresDasCrias()
+ {
+    let cores =  corAleatoria();
+    let {meusBebes} = minhasCrias();
+         meusBebes.forEach((elemento)=>{elemento.style.color = cores;});
+ };
+
+function posiçaoCrias()
+ {
+   let conteinerCrias = document.getElementById("bebes")
+   let posiçaoConteinerCrias = conteinerCrias.getBoundingClientRect();
+       return {posiçaoConteinerCrias, conteinerCrias};   
+ } 
+
+function animaçaoCrias()
+ {
+   let animaçao;  
+   let {posiçaoConteinerCrias, conteinerCrias} =  posiçaoCrias();
+   let x = posiçaoConteinerCrias.left;
+   let y = posiçaoConteinerCrias.right;
+   let z = posiçaoConteinerCrias.bottom;
+
+   if( posiçaoConteinerCrias.left <1000)
+    {
+       conteinerCrias.style.left = x + 1 + "px";   
+    }
+   else if( posiçaoConteinerCrias.left == 100)
+    {
+        conteinerCrias.style.left = x -1 + "px";
+    }
+      
+       //animaçao = requestAnimationFrame(animaçaoCrias) 
+    
+       console.log(posiçaoConteinerCrias.left)
+       console.log(posiçaoConteinerCrias)
+ } 
+ 
+ function man()
+ {
+    animaçaoCrias()
+    posiçaoCrias()
+    setInterval(coresDasCrias, 100);
+   
+ }
+
+ man();
+ 
   
