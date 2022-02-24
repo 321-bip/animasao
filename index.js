1e171e1682fdf2716c4a59a0e437ead40584324c
321-bip <dominicmarcos80@gmail.com>
2022-02-23 21:58:55 -0300

    finalisado

--- index.js
+++ index.js
@@ -20,42 +20,25 @@ function coresDasCrias()
          meusBebes.forEach((elemento)=>{elemento.style.color = cores;});
  };
 
-function posiçaoCrias()
+function numeroAliatorios()
  {
-   let conteinerCrias = document.getElementById("bebes")
-   let posiçaoConteinerCrias = conteinerCrias.getBoundingClientRect();
-       return {posiçaoConteinerCrias, conteinerCrias};   
- } 
-
-function animaçaoCrias()
+    let esquerda = Math.floor(Math.random() * 1000);
+    let altura =  Math.floor(Math.random() * 300);
+        return {esquerda, altura};
+ }
+ 
+function posisaoAleatoriaConteiner()
  {
-   let animaçao;  
-   let {posiçaoConteinerCrias, conteinerCrias} =  posiçaoCrias();
-   let x = posiçaoConteinerCrias.left;
-   let y = posiçaoConteinerCrias.right;
-   let z = posiçaoConteinerCrias.bottom;
-
-   if( posiçaoConteinerCrias.left <1000)
-    {
-       conteinerCrias.style.left = x + 1 + "px";   
-    }
-   else if( posiçaoConteinerCrias.left == 100)
-    {
-        conteinerCrias.style.left = x -1 + "px";
-    }
-      
-       //animaçao = requestAnimationFrame(animaçaoCrias) 
-    
-       console.log(posiçaoConteinerCrias.left)
-       console.log(posiçaoConteinerCrias)
+     let {esquerda, altura} = numeroAliatorios();
+     let conteiner = document.getElementById("bebes");
+         conteiner.style.left = esquerda + 'px';
+         conteiner.style.top = altura + 'px';
  } 
  
- function man()
+function man()
  {
-    animaçaoCrias()
-    posiçaoCrias()
-    setInterval(coresDasCrias, 100);
-   
+    setInterval(posisaoAleatoriaConteiner,900); 
+    setInterval(coresDasCrias, 900);
  }
 
  man();
